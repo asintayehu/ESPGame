9e8eec54b7a27cd5608d241097787c9113d13562
Aaron <asintayehu@proton.me>
2024-06-15 18:22:39 -0400

    im so cracked

--- Assignment1/src/ESPGame.java
+++ Assignment1/src/ESPGame.java
@@ -1,8 +1,28 @@
+import java.util.*;
+import java.io.*;
 
 public class ESPGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String filename;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter the filename: ");
+		
+		filename = keyboard.next();
+		
+		try {
+			File file = new File(filename);
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			
+			
+		}catch(IOException e)
+		
+		{
+			e.printStackTrace();
+		}
+		
+
 
 	}
 
